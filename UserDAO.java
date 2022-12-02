2807e29b82448d9c47e229887ba065d9e7b22fb8
bsh-ko <68588469+bsh-ko@users.noreply.github.com>
2022-12-02 13:52:16 +0900

    로그인

---
+++ UserDAO.java
@@ -0,0 +1,220 @@
+package model.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.User;
+
+/**
+ * 사용자 관리를 위해 데이터베이스 작업을 전담하는 DAO 클래스
+ * USER_INFO 테이블에 사용자 정보를 추가, 수정, 삭제, 검색 수행 
+ */
+public class UserDAO {
+	private JDBCUtil jdbcUtil = null;
+
+	public UserDAO() {			
+		jdbcUtil = new JDBCUtil();	// JDBCUtil 객체 생성
+	}
+
+	/**
+	 * 사용자 관리 테이블에 새로운 사용자 생성.
+	 */
+	public int create(User user) throws SQLException {
+		String sql = "INSERT INTO USER_INFO (ID, PASSWORD, NAME, PHONENUMBER, EMAIL, ADDRESS, BIRTH, nickname, POINT, MEETING, REGDATE)"
+				+ " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";	
+		Object[] param = new Object[] {user.getId(), user.getPassword(), 
+				user.getName(), user.getphoneNumber(), user.getEmail(), user.getAddress(), user.getBirth(), user.getNickname(),
+				0, "NULL", "2020/02/02"};		//포인트와 meeting 0과 null로 표시		
+		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil 에 insert문과 매개 변수 설정
+		System.out.println(user.getName() + user.getNickname()+ user.getBirth()+ user.getphoneNumber()+ user.getEmail());
+		try {				
+			int result = jdbcUtil.executeUpdate();	// insert 문 실행
+			return result;
+		} catch (Exception ex) {
+			jdbcUtil.rollback();
+			ex.printStackTrace();
+		} finally {		
+			jdbcUtil.commit();
+			jdbcUtil.close();	// resource 반환
+		}		
+		return 0;			
+	}
+
+	/**
+	 * 기존의 사용자 정보를 수정.
+	 */
+	public int update(User user) throws SQLException {
+		String sql = "UPDATE USER_INFO "
+				+ "SET password=?, name=?, nickname=?, birth=?, phoneNumber=?, email=? "
+				+ "WHERE Id=?";
+		Object[] param = new Object[] {user.getPassword(), user.getName(), user.getphoneNumber(),
+				user.getNickname(), user.getBirth(), user.getEmail(), 
+				user.getId()};				
+		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil에 update문과 매개 변수 설정
+
+		try {				
+			int result = jdbcUtil.executeUpdate();	// update 문 실행
+			return result;
+		} catch (Exception ex) {
+			jdbcUtil.rollback();
+			ex.printStackTrace();
+		}
+		finally {
+			jdbcUtil.commit();
+			jdbcUtil.close();	// resource 반환
+		}		
+		return 0;
+	}
+
+	/**
+	 * 사용자 Id에 해당하는 사용자를 삭제.
+	 */
+	public int remove(String Id) throws SQLException {
+		String sql = "DELETE FROM USER_INFO WHERE Id=?";		
+		jdbcUtil.setSqlAndParameters(sql, new Object[] {Id});	// JDBCUtil에 delete문과 매개 변수 설정
+
+		try {				
+			int result = jdbcUtil.executeUpdate();	// delete 문 실행
+			return result;
+		} catch (Exception ex) {
+			jdbcUtil.rollback();
+			ex.printStackTrace();
+		}
+		finally {
+			jdbcUtil.commit();
+			jdbcUtil.close();	// resource 반환
+		}		
+		return 0;
+	}
+
+	/**
+	 * 주어진 사용자 Id에 해당하는 사용자 정보를 데이터베이스에서 찾아 User 도메인 클래스에 
+	 * 저장하여 반환.
+	 */
+	public User findUser(String Id) throws SQLException {
+		String sql = "SELECT password, name, phoneNumber, email, address, birth, nickname "
+				+ "FROM USER_INFO "
+				+ "WHERE Id=? ";              
+		jdbcUtil.setSqlAndParameters(sql, new Object[] {Id});	// JDBCUtil에 query문과 매개 변수 설정
+
+		try {
+			ResultSet rs = jdbcUtil.executeQuery();		// query 실행
+			if (rs.next()) {						// 학생 정보 발견
+				User user = new User(		// User 객체를 생성하여 학생 정보를 저장
+						Id,
+						rs.getString("password"),
+						rs.getString("name"),
+						rs.getString("phoneNumber"),
+						rs.getString("email"),
+						rs.getString("birth"),
+						rs.getString("address"),
+						rs.getString("nickname"));
+				System.out.println("유저 정보 저장 성공");
+				return user;
+			}
+			System.out.println("유저 정보 저장 실패");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			jdbcUtil.close();		// resource 반환
+		}
+		return null;
+	}
+
+//	/**
+//	 * 전체 사용자 정보를 검색하여 List에 저장 및 반환
+//	 */
+	public List<User> findUserList() throws SQLException {
+		String sql = "SELECT Id, password, name, phoneNumber, email, address, birth, nickname " 
+				+ "FROM USER_INFO "
+				+ "ORDER BY Id";
+		jdbcUtil.setSqlAndParameters(sql, null);		// JDBCUtil에 query문 설정
+
+		try {
+			ResultSet rs = jdbcUtil.executeQuery();			// query 실행			
+			List<User> userList = new ArrayList<User>();	// User들의 리스트 생성
+			while (rs.next()) {
+				User user = new User(			// User 객체를 생성하여 현재 행의 정보를 저장
+						rs.getString("Id"),
+						rs.getString("password"),
+						rs.getString("name"),
+						rs.getString("phoneNumber"),
+						rs.getString("email"),
+						rs.getString("address"),
+						rs.getString("birth"),
+						rs.getString("nickname"));
+				userList.add(user);				// List에 User 객체 저장
+			}		
+			return userList;					
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			jdbcUtil.close();		// resource 반환
+		}
+		return null;
+	}
+//
+//	/**
+//	 * 전체 사용자 정보를 검색한 후 현재 페이지와 페이지당 출력할 사용자 수를 이용하여
+//	 * 해당하는 사용자 정보만을 List에 저장하여 반환.
+//	 */
+	public List<User> findUserList(int currentPage, int countPerPage) throws SQLException {
+		String sql = "SELECT Id, name, email, NVL(commId, 0) AS commId, cName " 
+				+ "FROM USER_INFO u LEFT OUTER JOIN Community c ON u.commId = c.cId "
+				+ "ORDER BY Id";
+		jdbcUtil.setSqlAndParameters(sql, null,					// JDBCUtil에 query문 설정
+				ResultSet.TYPE_SCROLL_INSENSITIVE,				// cursor scroll 가능
+				ResultSet.CONCUR_READ_ONLY);						
+
+		try {
+			ResultSet rs = jdbcUtil.executeQuery();				// query 실행			
+			int start = ((currentPage-1) * countPerPage) + 1;	// 출력을 시작할 행 번호 계산
+			if ((start >= 0) && rs.absolute(start)) {			// 커서를 시작 행으로 이동
+				List<User> userList = new ArrayList<User>();	// User들의 리스트 생성
+				do {
+					User user = new User(			// User 객체를 생성하여 현재 행의 정보를 저장
+							rs.getString("Id"),
+							rs.getString("password"),
+							rs.getString("name"),
+							rs.getString("phoneNumber"),
+							rs.getString("email"),
+							rs.getString("address"),
+							rs.getString("birth"),
+							rs.getString("nickname"));
+					userList.add(user);							// 리스트에 User 객체 저장
+				} while ((rs.next()) && (--countPerPage > 0));		
+				return userList;							
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			jdbcUtil.close();		// resource 반환
+		}
+		return null;
+	}
+
+	/**
+	 * 주어진 사용자 Id에 해당하는 사용자가 존재하는지 검사 
+	 */
+	public boolean existingUser(String Id) throws SQLException {
+		String sql = "SELECT count(*) FROM USER_INFO WHERE Id=?";      
+		jdbcUtil.setSqlAndParameters(sql, new Object[] {Id});	// JDBCUtil에 query문과 매개 변수 설정
+
+		try {
+			ResultSet rs = jdbcUtil.executeQuery();		// query 실행
+			if (rs.next()) {
+				int count = rs.getInt(1);
+				return (count == 1 ? true : false);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			jdbcUtil.close();		// resource 반환
+		}
+		return false;
+	}
+
+}
---
+++ registerForm.jsp
@@ -0,0 +1,152 @@
+<%@page contentType="text/html; charset=utf-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+<title>에코로그 - 회원 가입</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel=stylesheet href="<c:url value='/css/user.css' />"
+	type="text/css">
+<script>
+function userCreate() {
+	
+	if (form.Id.value == "") {
+		alert("사용자 ID를 입력하십시오.");
+		form.Id.focus();
+		return false;
+	} 
+	if (form.password.value == "") {
+		alert("비밀번호를 입력하십시오.");
+		form.password.focus();
+		return false;
+	}
+	if (form.password.value != form.password2.value) {
+		alert("비밀번호가 일치하지 않습니다.");
+		form.name.focus();
+		return false;
+	}
+	if (form.name.value == "") {
+		alert("이름을 입력하십시오.");
+		form.name.focus();
+		return false;
+	}
+	if(form.phoneNumber.value =="") {
+		alert("전화번호를 입력하십시오.");
+		form.phoneNumber.focus();
+		return false;
+	}
+	var emailExp = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+	if(emailExp.test(form.email.value)==false) {
+		alert("이메일 형식이 올바르지 않습니다.");
+		form.email.focus();
+		return false;
+	}
+	if (form.address.value == "") {
+		alert("주소를 입력하십시오.");
+		form.nickName.focus();
+		return false;
+	}
+	if (form.birth.value == "") {
+		alert("생년월일을 입력하십시오.");
+		form.birth.focus();
+		return false;
+	}
+	if (form.nickName.value == "") {
+		alert("닉네임을 입력하십시오.");
+		form.nickName.focus();
+		return false;
+	}
+	form.submit();
+}
+function userList(targetUri) {
+	form.action = targetUri;
+	form.submit();
+}
+
+</script>
+</head>
+<body>
+	<br>
+	<!-- registration form  -->
+	<form name="form" method="POST"
+		action="<c:url value='/user/register' />">
+		<table style="width: 100%">
+			<tr>
+				<td width="100" align="center"></td>
+				<td>
+					<table>
+						<tr>
+							<td class="title">&nbsp;&nbsp;<b>에코로그 - 회원 가입</b>&nbsp;&nbsp;
+							</td>
+						</tr>
+					</table> <!-- 회원가입이 실패한 경우 exception 객체에 저장된 오류 메시지를 출력 --> <c:if
+						test="${registerFailed}">
+						<font color="red"><c:out value="${exception.getMessage()}" /></font>
+					</c:if> <br>
+					<table style="background-color: YellowGreen">
+						<tr height="40">
+							<td width="700" align="center" bgcolor="E6ECDE">사용자 ID</td>
+							<td width="700" bgcolor="ffffff" style="padding-left: 10"><input
+								type="text" style="width: 240;" name="Id"></td>
+						</tr>
+						<tr height="40">
+							<td width="700" align="center" bgcolor="E6ECDE">비밀번호</td>
+							<td width="700" bgcolor="ffffff" style="padding-left: 10"><input
+								type="password" style="width: 240" name="password"></td>
+						</tr>
+						<tr height="40">
+							<td width="700" align="center" bgcolor="E6ECDE">비밀번호 확인</td>
+							<td width="700" bgcolor="ffffff" style="padding-left: 10"><input
+								type="password" style="width: 240" name="password2"></td>
+						</tr>
+						<tr height="40">
+							<td width="700" align="center" bgcolor="E6ECDE">이름</td>
+							<td width="700" bgcolor="ffffff" style="padding-left: 10"><input
+								type="text" style="width: 240" name="name">
+							</td>
+						</tr>
+						<tr height="40">
+							<td width="700" align="center" bgcolor="E6ECDE">닉네임</td>
+							<td width="700" bgcolor="ffffff" style="padding-left: 10"><input
+								type="text" style="width: 240" name="nickName">
+							</td>
+						</tr>
+						<tr height="40">
+							<td width="700" align="center" bgcolor="E6ECDE">생년월일</td>
+							<td width="700" bgcolor="ffffff" style="padding-left: 10">
+							<input type="text" name="birth" maxlength="8" placeholder="ex)19990101"
+							size="6"> 
+							</td>
+						</tr>
+						<tr height="40">
+							<td width="700" align="center" bgcolor="E6ECDE">이메일 주소</td>
+							<td width="700" bgcolor="ffffff" style="padding-left: 10"><input
+								type="text" style="width: 240" name="email">
+							</td>
+						</tr>
+						<tr height="40">
+							<td width="700" align="center" bgcolor="E6ECDE">전화번호</td>
+							<td width="700" bgcolor="ffffff" style="padding-left: 10"><input
+								type="text" style="width: 240" name="phoneNumber">
+							</td>
+						</tr>
+						<tr height="40">
+							<td width="700" align="center" bgcolor="E6ECDE">주소</td>
+							<td width="700" bgcolor="ffffff" style="padding-left: 10"><input
+								type="text" style="width: 240" name="address">
+							</td>
+						</tr>
+					</table> <br>
+					<table style="width: 100%">
+						<tr>
+							<td align="left"><input type="button" value="회원 가입"
+								onClick="userCreate()"> &nbsp; <input type="button"
+								value="로그인 화면"
+								onClick="userList('<c:url value='/user/login' />')"></td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
